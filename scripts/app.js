4ac14881e21094e7217d8bbe363f287a5b2eb8fa
VicentiuBacioiu <vicentiu.bacioiu@gmail.com>
2016-01-05 18:39:09 +0200

    adding tags

--- scripts/app.js
+++ scripts/app.js
@@ -1,16 +1,31 @@
-(function(){
+(function () {
   var tdb = angular.module('box', []);
 
   var taskList = [{
     title: 'Task 1',
-    content: 'Task Content 1'
-  }, 
-  {
+    content: 'Task Content 1',
+    tags: ['Personal']
+  }, {
     title: 'Task 2',
-    content: 'Task Content 2'
+    content: 'Task Content 2',
+    tags: ['Personal', 'Important']
+  }, {
+    title: 'Task 3',
+    content: 'Task Content 3',
+    tags: ['Work']
   }];
 
-  tdb.controller('ItemController', function(){
+  var tagsList = [
+    'Personal',
+    'Work',
+    'Important'
+  ];
+
+  tdb.controller('ItemController', function () {
     this.tasks = taskList;
+  });
+
+  tdb.controller('TagController', function () {
+    this.tags = tagsList;
   });
 })();
